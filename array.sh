658236c4386d7755f721c847f2645f3fa2609fe5
varsha <dhanya312@gmail.com>
2019-03-17 11:32:19 -0500

    adding arrayscript

---
+++ array.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "program for listing array"
+arr[0]="dhanya"
+arr[1]="sreedhar"
+arr[2]="varsha"
+arr[3]="abhilash"
+arr[4]="vandhichal"
+arr[5]="rajan"
+
+echo ${arr[0]}
+echo ${arr[1]}
+echo ${arr[2]}
+echo ${arr[3]}
+echo ${arr[4]}
+echo ${arr[5]}
+echo "---------"
